05ecd8e1b6efd5abe5ab667f8ee19dad8299ea5b
Marcela Ribeiro de Oliveira <marcelaribeirooliveira@gmail.com>
2022-12-19 19:31:33 -0300

    addees belongs to relations to lauch model and added status as enum

--- app/models/launch.rb
+++ app/models/launch.rb
@@ -1,3 +1,7 @@
 class Launch < ApplicationRecord
-  belongs_to :rocket
+  belongs_to :rocket, optional: true
+  belongs_to :launch_status, optional: true
+  belongs_to :launch_service_provider, optional: true
+  belongs_to :mission, optional: true
+  enum status: [:draft, :trash, :published]
 end
